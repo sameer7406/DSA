431cccc0954677762689aadccf39acb91de73860
Sameer Sharma <sameer7406sa@gmail.com>
2026-08-26 23:41:48 +0530

    Time: 3 ms (66.91%) | Memory: 15.1 MB (12.31%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // // int num;
+        // for (int i=0;i< nums.size();i++){
+        //     for (int j=i-1;j<=nums.size();j++){
+        //         if (nums[i]+nums[j]==target){
+        //             return {i,j};
+        //             }
+        //     }
+        // }
+
+        int needed,i;
+        unordered_map<int,int> mp;
+        for (int i=0;i<nums.size();i++){
+            needed =target-nums[i];
+        
+        if(mp.find(needed)!=mp.end()){
+            return {mp[needed],i};
+        }
+        mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
