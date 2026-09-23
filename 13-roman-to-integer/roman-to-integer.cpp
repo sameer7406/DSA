59221935b89a63dd11aa96d1aaf24c9eb6c7de60
Sameer Sharma <sameer7406sa@gmail.com>
2026-09-23 22:25:32 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (86.01%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        int sum = 0;
+
+        for(int i=0; i<s.length(); i++) {
+
+            int curr;
+
+            if(s[i]=='I') curr=1;
+            else if(s[i]=='V') curr=5;
+            else if(s[i]=='X') curr=10;
+            else if(s[i]=='L') curr=50;
+            else if(s[i]=='C') curr=100;
+            else if(s[i]=='D') curr=500;
+            else curr=1000;
+
+            if(i+1 < s.length()) {
+
+                int next;
+
+                if(s[i+1]=='I') next=1;
+                else if(s[i+1]=='V') next=5;
+                else if(s[i+1]=='X') next=10;
+                else if(s[i+1]=='L') next=50;
+                else if(s[i+1]=='C') next=100;
+                else if(s[i+1]=='D') next=500;
+                else next=1000;
+
+                if(curr < next)
+                    sum -= curr;
+                else
+                    sum += curr;
+
+            } else {
+                sum += curr;
+            }
+        }
+
+        return sum;
+    }
+};
